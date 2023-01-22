29cec56326df79d88b3ab24be781a8325fc23922
samoorai <hirotakatech@gmail.com>
2023-01-22 17:49:05 +0100

    Added initial layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import '@picocss/pico'
+</script>
+<nav class="container">
+  <ul>
+    <li><strong>Pocketcash</strong></li>
+  </ul>
+  <ul>
+    <li><a href="#">Add an expanse</a></li>
+    <li><a href="#">Register</a></li>
+    <li><a href="#" role="button">Login</a></li>
+  </ul>
+</nav >
+<main  class="container">
+    <slot />
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<table data-theme="dark">
+  <thead>
+    <tr>
+      <th scope="col">Time</th>
+      <th scope="col">Name</th>
+      <th scope="col">Cost</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>2022/11/03</td>
+      <td>48 Laws of Power</td>
+      <td>11.99$</td>
+    </tr>
+    <tr>
+      <td>2022/10/03</td>
+      <td>12 Rules of Life</td>
+      <td>15.23$</td>
+    </tr>
+  <tfoot>
+    <tr>
+      <td scope="col">November</td>
+      <td scope="col"></td>
+      <td scope="col">27.22$</td>
+    </tr>
+  </tfoot>
+</table>
